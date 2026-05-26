ef42e04874816623f8321a57c41b509b7a0193cb
Ananya-yash <ananyayashbhatnagar@gmail.com>
2026-05-26 16:58:07 +0530

    db

--- db.js
+++ db.js
@@ -2,12 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  connectionTimeoutMillis: 10000, // ← ADD THIS
-  idleTimeoutMillis: 30000,       // ← ADD THIS
+  ssl: { rejectUnauthorized: false }
 });
 
-// Test connection immediately
 pool.connect((err, client, release) => {
   if (err) {
     console.error('❌ Connection error:', err.message);
